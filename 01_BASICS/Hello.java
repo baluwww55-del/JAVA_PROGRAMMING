1cf99b2399f9a31d378db8984dae15dc6c171886
Balu <balu.www.55@gmail.com>
2026-08-21 10:05:17 +0530

    Add files via upload

---
+++ 01_BASICS/Hello.java
@@ -0,0 +1,20 @@
+// Java is an object oriented programming language . 
+// it uses major concept of OOPS known as pillors of oops (object oriented programming language :)
+
+public class Hello {  // here we have defined the class hello    
+    public static void main(String[] args) {   // we have used public variable and void main to start the execution of the program .
+        System.out.println("Balaji");       // here we are printing the output for  the class we have created 
+    }
+}                                             // terminated the program by closing the flower bracket . 
+
+// this is the simple structure used to code in java using oops concept 
+
+// there are the four major concepts in oops 
+// 1) Inheritance .
+// 2) Abstraction .
+// 3) Encapsulation .
+// 4) Polymorphism .
+
+// this is the major concepts of oops using in java language .
+
+
